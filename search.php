488b6462c4eeb99202eebc766c2b09b0574934ca
Zhanelya Subebayeva <subebo91@inbox.ru>
2014-03-14 16:12:51 +0000

    Added isFriend indication for search

--- search.php
+++ search.php
@@ -6,16 +6,28 @@
         $fullname = $_GET['name'];
         $name = explode(" ", $fullname);
         $sql_select = "SELECT p.fname, p.lname, p.acc_id
-                       FROM profile p 
-                       WHERE UPPER(p.fname)=UPPER('".$name[0]."')
-                          OR UPPER(p.lname)=UPPER('".$name[0]."')
+                       FROM profile p
+                       WHERE  UPPER(p.fname)=UPPER('".$name[0]."')
+                          OR  UPPER(p.lname)=UPPER('".$name[0]."')
                        ORDER BY p.fname ASC";
         $stmt = $conn->query($sql_select);
         $people=$stmt->fetchAll(); 
         if(count($people) > 0) {
             foreach($people as $person)
               {
-                echo "<div class=\"row-fluid friends_block\"><h5><a href=\"..#/p_profile?".$person['acc_id']."/\">".$person['fname']." ".$person['lname']."</a></h5></div>";
+                $sql_select = " SELECT f.user1_id
+                                FROM friendship f
+                                WHERE  f.user1_id = '".$person['acc_id']."'
+                                   OR  f.user2_id = '".$person['acc_id']."'";
+                 $stmt = $conn->query($sql_select);
+                 $arefriends=$stmt->fetchAll();          
+                 echo "<div class=\"row-fluid friends_block\"><h5><a href=\"..#/p_profile?".$person['acc_id']."/\">".$person['fname']." ".$person['lname']."</a>";
+                 if(count ($arefriends)>0){
+                     echo "<button id=\"are_friends\">Friends</button>";
+                 }else{
+                     echo "<button id=\"add_friend\">+1 Add Friend</button>";
+                 }
+                 echo "</h5></div>";
               }
         }
     }catch(Exception $e) {
